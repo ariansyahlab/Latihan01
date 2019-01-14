ef3cf562a0b372dcfbafdbc13a9b26ee09b0a584
ariansyahlab <ariansyahlab@gmail.com>
2019-01-15 00:11:13 +0700

    integrated vue

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -11,11 +11,13 @@ scratch. This page gets rid of all links and provides the needed markup only.
 
   <title>{{ config('app.name', 'Laravel') }}</title>
 
+  <meta name="csrf-token" content="{{ csrf_token() }}">
+
   <link rel="stylesheet" href="/css/app.css">  
   
 </head>
 <body class="hold-transition sidebar-mini">
-<div class="wrapper">
+<div class="wrapper" id="app">
 
   <!-- Navbar -->
   <nav class="main-header navbar navbar-expand bg-white navbar-light border-bottom">
@@ -71,12 +73,12 @@ scratch. This page gets rid of all links and provides the needed markup only.
                with font-awesome or any other icon font library -->
             
             <li class="nav-item">
-                <a href="#" class="nav-link">
+                <router-link to="/dashboard" class="nav-link">
                     <i class="nav-icon fas fa-tachometer-alt"></i>
                     <p>
                         Dashboard
                     </p>
-                </a>
+                </router-link>
             </li>
 
           <li class="nav-item has-treeview">
@@ -104,12 +106,12 @@ scratch. This page gets rid of all links and provides the needed markup only.
           </li>
           
             <li class="nav-item">
-                <a href="#" class="nav-link">
+                <router-link to="/profile" class="nav-link">
                     <i class="nav-icon fas fa-user"></i>
                     <p>
                         Profile
                     </p>
-                </a>
+                </router-link>
             </li>
 
             <li class="nav-item">
@@ -135,6 +137,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
     <!-- Main content -->
     <div class="content">
       <div class="container-fluid">
+      <router-view></router-view>
 
       </div><!-- /.container-fluid -->
     </div>
